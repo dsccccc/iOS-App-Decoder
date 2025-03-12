01e58367c340054a16c2c87d3b9f35aa9d196676
dsccccc <deshuchen97+dsc@gmail.com>
2025-03-12 14:05:21 +0800

    Update worker.py

--- script/worker.py
+++ script/worker.py
@@ -8,7 +8,7 @@ def replacer(line: str, input_file: str = './tmp/README.md', output_file: str = 
     with open(input_file, 'r') as f:
         data = f.read()
     print(line)
-    data = data.rstrip('\n') + f"\n|{line[:emoji.emoji_list(free)[0]['match_start']]}|{free}{line[emoji.emoji_list(free)[0]['match_end']:]}|"
+    data = data.rstrip('\n') + f"\n|{line[:emoji.emoji_list(free)[0]['match_start']]}|{line[emoji.emoji_list(free)[0]['match_end']:]}|"
     with open(output_file, 'w') as f:
         f.write(data)
 
